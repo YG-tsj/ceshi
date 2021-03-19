563dd722253c817c9832229cb24fe9982a394c6e
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-19 10:04:59 +0800

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -5,22 +5,27 @@ apt update && apt install curl sudo lsb-release iptables -y
 if grep -q -E -i "debian" /etc/issue; then
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
-
-elif grep -q -E -i "ubuntu" /etc/issue; then
-echo apt update
-
-else 
-echo -e "MUST Ubuntu20.04 and Debain10"
-exit 0
-
-fi
-
 apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
 curl -fsSL git.io/wireguard-go.sh | sudo bash
 curl -fsSL git.io/wgcf.sh | sudo bash
 echo | wgcf register
 wgcf generate
+
+elif grep -q -E -i "ubuntu" /etc/issue; then
+apt install net-tools iproute2 openresolv dnsutils -y
+apt install wireguard-tools --no-install-recommends
+curl -fsSL git.io/wireguard-go.sh | sudo bash
+curl -fsSL git.io/wgcf.sh | sudo bash
+echo | wgcf register
+wgcf generate
+
+else 
+   echo -e "MUST Ubuntu20.04 and Debain10"
+exit 0
+
+fi
+
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 sed -i 's/1.1.1.1/2620:fe::10,2001:4860:4860::8888,2606:4700:4700::1111/g' wgcf-profile.conf
 sed -i '/\:\:\/0/d' wgcf-profile.conf
