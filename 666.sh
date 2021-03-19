666272b19b8470c6375b010a430f7fcc9485b8e3
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-19 10:52:03 +0800

    Update 666.sh

--- 666.sh
+++ 666.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if grep -q -E -i "debian" /etc/issue; then
 apt update && apt install curl sudo lsb-release iptables -y
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
@@ -10,13 +9,6 @@ curl -fsSL git.io/wireguard-go.sh | sudo bash
 curl -fsSL git.io/wgcf.sh | sudo bash
 echo | wgcf register
 wgcf generate
-
- else 
-	echo -e "只支持UB"
-	exit 0
-
-fi
-
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 sed -i 's/1.1.1.1/2620:fe::10,2001:4860:4860::8888,2606:4700:4700::1111/g' wgcf-profile.conf
 sed -i '/\:\:\/0/d' wgcf-profile.conf
