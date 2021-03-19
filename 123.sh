d8544f7570aafee6ba97e747266cc774e32c70c6
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-19 09:01:53 +0800

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
- if grep -q -E -i "debian" /etc/issue; then
 apt update && apt install curl sudo lsb-release iptables -y
+
+ if grep -q -E -i "debian" /etc/issue; then
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
 
@@ -12,7 +13,7 @@ elif grep -q -E -i "ubuntu" /etc/issue; then
 	exit 0
 
 fi
-apt update && apt install curl sudo lsb-release iptables -y
+
 apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
 curl -fsSL git.io/wireguard-go.sh | sudo bash
