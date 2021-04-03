d7c407c8ee2b2cbe8a0c0491650029e76cd871d9
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-03 18:10:49 +0800

    Update 11.sh

--- 11.sh
+++ 11.sh
@@ -3,20 +3,20 @@ echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo
 apt update
 apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
-wget https://bitbucket.org/ygtsj/euserv-warp/raw/8cccfd4ba639a5fa3a784e1ae37efb30e58310e4/wgcf
-wget https://bitbucket.org/ygtsj/euserv-warp/raw/8cccfd4ba639a5fa3a784e1ae37efb30e58310e4/wireguard-go
+curl -fsSL git.io/wgcf.sh | sudo bash
+curl -fsSL git.io/wireguard-go.sh | sudo bash
 cp wireguard-go /usr/bin
 cp wgcf /usr/local/bin/wgcf
 chmod +x /usr/local/bin/wgcf
 chmod +x /usr/bin/wireguard-go
 echo | wgcf register
 wgcf generate
-sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
-sed -i '/\:\:\/0/d' wgcf-profile.conf
+sed -i 's/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf
+sed -i '/0\.0\.0\.0\/0/d' wgcf-profile.conf
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
-rm -f srvDIG9* wgcf* wireguard-go*
-grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
+rm -f 11* wgcf* wireguard-go*
+grep -qE '^[ ]*label[ ]*2002::/16[ ]*2' /etc/gai.conf || echo 'label 2002::/16   2' | sudo tee -a /etc/gai.conf
 echo -e "检测是否优先启动Warp IPV4地址：如果下方显示为8.2X……开头的IPV4地址，就说明成功啦！……如果是2a02开头的IPV6地址，那就再重新运行脚本吧"
 curl ip.p3terx.com
