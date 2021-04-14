af5b795cc742ed962db7bbe7f1f076891ad6c1b0
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-14 20:19:37 +0800

    Update 60.sh

--- 60.sh
+++ 60.sh
@@ -12,7 +12,7 @@ chmod +x /usr/bin/wireguard-go
 echo | wgcf register
 wgcf generate
 echo "请输入要本地IP 例：202a...../128 --->"
-echo | read -p "域名:" eu6
+read -p "域名:" eu6
 echo "$eu6"
 sed -i "5 s/^/PostUp = ip -6 rule add from 2a02:180:6:1::2a3b/128 table main\n/" wgcf-profile.conf
 sed -i '6 s/^/PostDown = ip -6 rule delete from 2a02:180:6:1::2a3b/128 table main\n/' wgcf-profile.conf
