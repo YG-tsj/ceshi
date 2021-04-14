d3a4108fa7f81d9dd5fe91f3b8419c3a90d03e07
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-14 20:58:39 +0800

    Update and rename 61.sh to 66.sh

--- 66.sh
+++ 66.sh
@@ -13,9 +13,9 @@ echo | wgcf register
 wgcf generate
 echo "请输入要本地IP 例：202a...../128 --->"
 read -p "域名:" eu6
-echo "$eu6"
-sed -i "5 s/^/PostUp = ip -6 rule add from 2a02:180:6:1::2a3b/128 table main\n/" wgcf-profile.conf
-sed -i '6 s/^/PostDown = ip -6 rule delete from 2a02:180:6:1::2a3b/128 table main\n/' wgcf-profile.conf
+echo $eu6
+sed -i "5 s/^/PostUp = ip -6 rule add from $eu6 table main\n/" wgcf-profile.conf
+sed -i "6 s/^/PostDown = ip -6 rule delete from $eu6 table main\n/" wgcf-profile.conf
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
