d1433b8b019023287f0d44f77200cd9c61c4a944
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-18 07:46:18 +0800

    Update and rename 34.sh to 31.sh

--- 31.sh
+++ 31.sh
@@ -1,6 +1,6 @@
-echo -e "\033[1;36m 请 注 意！！！甬 哥 的 脚 本 仅 支 持 Ubuntu 与 Debain 系 统！！！Warp 仅 添 加 并 接 管 IPV6！！！ \033[0m"
+echo -e "\033[1;36m 请 注 意！！！甬 哥 的 脚 本 仅 支 持 Ubuntu 与 Debain 系 统！！！Warp  添 加 并 接 管 IPV6+4！！！ \033[0m"
 apt update 
-apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
+apt install wireguard-tools -y
 wget -N https://cdn.jsdelivr.net/gh/YG-tsj/EUserv-addv4-warp/wgcf
 cp wgcf /usr/local/bin/wgcf
 chmod +x /usr/local/bin/wgcf
@@ -15,6 +15,5 @@ cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
-grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
 echo -e "\033[1;33m 检测是否成功启动（IPV4+IPV6）双栈Warp！\n 显示IPV4地址：$(wget -qO- ipv4.ip.sb) 显示IPV6地址：$(wget -qO- ipv6.ip.sb) \033[0m"
 echo -e "\033[1;32m 如上方显示IPV4地址：8.2…………，IPV6地址：2a09:…………，则说明成功啦！\n 如上方IPV4无IP显示,IPV6显示本地IP（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
