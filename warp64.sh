059f03229078f8caa55a64a3dde00d1f7ea13d58
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-17 09:52:23 +0800

    Update and rename 1000.sh to warp64.sh

--- warp64.sh
+++ warp64.sh
@@ -1,4 +1,4 @@
-echo -e "\033[1;36m 请 注 意！！！甬 哥 的 脚 本 仅 支 持 Ubuntu 与 Debain 系 统！Warp 接 管 IPV4 与 IPV6！ \033[0m"
+echo -e "\033[1;36m 请 注 意！！！甬 哥 的 脚 本 仅 支 持 Ubuntu 与 Debain 系 统！！！本 脚 本 双 栈 Warp 接 管 IPV4 与 IPV6！ \033[0m"
 apt update && apt install curl sudo lsb-release iptables -y
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
@@ -14,15 +14,15 @@ echo | wgcf register
 wgcf generate
 sed -i '5 s/^/PostUp = ip -6 rule add from eu6 table main\n/' wgcf-profile.conf
 sed -i '6 s/^/PostDown = ip -6 rule delete from eu6 table main\n/' wgcf-profile.conf
-echo "请输入德鸡EUserv本地IP+/128，可直接复制EUserv后台（Main IPv6-Address）信息，注意在非中文环境下复制，例：2a02:…………/128"
-read -p "粘贴IPV6+/128:" eu6
+echo -e "\033[1;31m 请复制德鸡EUserv本地IP+/128，可在EUserv后台（Main IPv6-Address）查看，注意在非中文环境下复制，例：2a02:…………/128 \033[0m"
+read -p "\033[1;35m 粘贴（IPV6+/128）: \033[0m" eu6
 sed -i "s#eu6#$eu6#g" wgcf-profile.conf
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
-rm -f warp4* wgcf* wireguard-go*
+rm -f warp64* wgcf* wireguard-go*
 grep -qE '^[ ]*label[ ]*2002::/16[ ]*2' /etc/gai.conf || echo 'label 2002::/16   2' | sudo tee -a /etc/gai.conf
 echo -e "\033[1;33m 检测是否成功启动（IPV4+IPV6）双栈Warp！\n 显示IPV4地址：$(wget -qO- ipv4.ip.sb) 显示IPV6地址：$(wget -qO- ipv6.ip.sb) \033[0m"
 echo -e "\033[1;32m 如上方显示IPV4地址：8.2…………，IPV6地址：2a09:…………，则说明成功啦！\n 如上方IPV4无IP显示,IPV6显示本地IP（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
