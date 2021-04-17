4c325facc167744745bafb6844b5eb51d0e0cc68
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-18 07:25:08 +0800

    Update and rename mi.sh to m.sh

--- mi.sh
+++
@@ -1,5 +0,0 @@
-read -p "ROOT密码:" mima
-echo root:$mima | sudo chpasswd root
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo service sshd restart
