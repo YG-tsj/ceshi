8b5f57e62564ce42688fcfc66276db8b4c2c4552
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-18 16:12:03 +0800

    Update and rename 123.sh to 1234.sh

--- 1234.sh
+++ 1234.sh
@@ -7,4 +7,4 @@ wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5
 wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
 sudo dpkg -i *.deb
 rm -rf /etc/iptables && reboot
-echo | reboot                           
+                          
