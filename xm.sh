8a730d0a89076069fcd738ef6eb270f68328b8b1
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-18 11:12:10 +0800

    Update xm.sh

--- xm.sh
+++ xm.sh
@@ -1,4 +1,5 @@
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
 wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
 sudo apt update && sudo apt install linux-xanmod -y
+rm -rf /etc/iptables
 reboot
