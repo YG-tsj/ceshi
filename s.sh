c9fb79303e88be10c538175904c03ee998e873ff
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-04 14:02:36 +0800

    Update and rename sss.sh to s.sh

--- s.sh
+++ s.sh
@@ -1,6 +1,6 @@
-read -p "自定义ROOT密码:" mima
 
-sudo sed -i "$a echo root:$mima | sudo chpasswd root" 123.txt
+
+sudo sed -i '$a echo root:$mima | sudo chpasswd root' 123.txt
 
 sudo echo "sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;" >> 123.txt
 
@@ -8,5 +8,8 @@ sudo echo "sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication ye
 
 sudo sed -i '$a sudo service sshd restart' 123.txt
 
-reboot
+read -p "自定义ROOT密码:" mima
+
+sudo sed -i "s#mima#$mima#g" 123.txt
+
 
