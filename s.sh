dd2f446270efe7d4059266c1a87b1ad9e2b07a4f
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-04 14:10:24 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,15 +1,15 @@
 
 
-sudo sed -i '$a echo root:$mima | sudo chpasswd root' 123.txt
+sudo sed -i '$a echo root:mima | sudo chpasswd root' /etc/cloud/cloud.cfg
 
-sudo echo "sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;" >> 123.txt
+sudo echo "sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;" >> /etc/cloud/cloud.cfg
 
-sudo echo "sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;" >> 123.txt
+sudo echo "sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;" >> /etc/cloud/cloud.cfg
 
-sudo sed -i '$a sudo service sshd restart' 123.txt
+sudo sed -i '$a sudo service sshd restart' /etc/cloud/cloud.cfg
 
 read -p "自定义ROOT密码:" mima
 
-sudo sed -i "s#mima#$mima#g" 123.txt
+sudo sed -i "s#mima#$mima#g" /etc/cloud/cloud.cfg
 
-
+sudo reboot
