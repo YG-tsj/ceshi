fd691928b079f16456aa006bd3056b6c2e66672f
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-05 20:31:49 +0800

    Update 22.sh

--- 22.sh
+++ 22.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "自定义ROOT密码:" mima
-echo root:$mima | sudo passwd root
+echo root:$mima | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
