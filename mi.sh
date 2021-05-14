6ab8f99b82a5d412027649046a98e695d35c4764
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-14 20:17:20 +0800

    Update mi.sh

--- mi.sh
+++ mi.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+main=`uname  -r | awk -F . '{print $1 }'`
+minor=`uname -r | awk -F . '{print $2}'`
+
+if [ "$main" -lt 5 ]|| [ "$minor" -lt 6 ]; then 
+	echo '内核太低，需要内核大于5.6 执行 wget -N "https://github.000060000.xyz/tcpx.sh" && chmod +x tcpx.sh && ./tcpx.sh 换内核吧'
+	exit 1
+fi
+
 sudo lsattr /etc/passwd /etc/shadow
 sudo chattr -i /etc/passwd /etc/shadow
 sudo chattr -a /etc/passwd /etc/shadow
