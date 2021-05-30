95961d6977e8558b99ebd49d963e78bc1c5f028b
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-30 13:01:00 +0800

    Update mim.sh

--- mim.sh
+++ mim.sh
@@ -1,14 +1,9 @@
 read -p "自定义ROOT密码:" mima
-
-sudo sed -i "$a echo root:$mima | sudo chpasswd root" /etc/cloud/cloud.cfg
-
-sudo sed -i '$a sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;' /etc/cloud/cloud.cfg
-
-sudo sed -i '$a sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;' /etc/cloud/cloud.cfg
-
-sudo sed -i '$a sudo service sshd restart' /etc/cloud/cloud.cfg
-
-reboot
+echo root:$mima | sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
+sudo reboot
 
 
 
