8da37e2479797fd0e367bfb38acf0a79bfcf2fe0
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-30 14:03:24 +0800

    Update and rename mim5.sh to mi9.sh

--- mi9.sh
+++ mi9.sh
@@ -1,4 +1,4 @@
-echo -e nameserver 2a01:4f8:c2c:123f::1 > /etc/resolv.conf
+
 read -p "自定义ROOT密码:" mima
 echo root:$mima | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
