e87bbe328feef9b40564cbc6e0d5502ac422b346
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-27 14:16:12 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -30,10 +30,10 @@ WireGuardStatus() {
 warpwg=$(systemctl status wg-quick@wgcf | grep Active | awk 'NR==1 {print $2}')
 case ${warpwg} in
 active)
-warpwg=yellow " 运行中 "
-;
+     warpwg=yellow " 运行中 "
+     ;
 *)
-warpwg=yellow " 未运行 "
-;
+     warpwg=yellow " 未运行 "
+     ;
 esac
 }
