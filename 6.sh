51ab1e481a8f0d1101a325f13bc2d31084e8834c
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-27 14:54:06 +0800

    Update and rename 5.sh to 6.sh

--- 6.sh
+++ 6.sh
@@ -29,10 +29,10 @@ rred(){
 warpwg=$(systemctl status wg-quick@wgcf | grep Active | awk 'NR==1 {print $2}')
 case ${warpwg} in
 active)
-     warpwg=yellow " 运行中 "
+     yellow " 运行中 "
      ;;
 *)
-     warpwg=yellow " 未运行 "
+     yellow " 未运行 "
 esac
 
 
