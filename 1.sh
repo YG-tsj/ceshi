3e2adffd33948d725c9ed8f040dfd18f4885d13d
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-27 14:16:33 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -31,9 +31,9 @@ warpwg=$(systemctl status wg-quick@wgcf | grep Active | awk 'NR==1 {print $2}')
 case ${warpwg} in
 active)
      warpwg=yellow " 运行中 "
-     ;
+     ;;
 *)
      warpwg=yellow " 未运行 "
-     ;
+     ;;
 esac
 }
