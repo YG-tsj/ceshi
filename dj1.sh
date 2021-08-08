19003834b9e8d2e2f14c498c00d034b86a7a14e1
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-08-08 14:19:53 +0800

    Update dj1.sh

--- dj1.sh
+++ dj1.sh
@@ -1,9 +1,10 @@
 apt install python3 python3-pip -y
 pip install requests beautifulsoup4
-
-read -p "自定义账户:" USERNAME
-read -p "自定义密码:" PASSWORD
+apt-get install cron 
+crontab -e
+* * * * * /usr/bin/python3 /root/main.py >/dev/null  2>&1
+service cron restart
 
 wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main.py
 chmod +x main.py
-python3 main.py $USERNAME $PASSWORD
+python3 main.py
