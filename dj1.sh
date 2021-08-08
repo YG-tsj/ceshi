41f458df80a6acb81d3e3ae1f54557eee447c4e4
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-08-08 13:26:48 +0800

    Update dj1.sh

--- dj1.sh
+++ dj1.sh
@@ -1,6 +1,9 @@
 apt install python3 python3-pip -y
 pip install requests beautifulsoup4
 
+read -p "自定义账户:" USERNAME
+read -p "自定义密码:" PASSWORD
+
 wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main.py
 chmod +x main.py
-python3 main.py
+python3 main.py $USERNAME $PASSWORD
