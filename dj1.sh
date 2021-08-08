c2bb9c508390f934ad5bedded0796a5eefb59a62
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-08-08 13:10:59 +0800

    Update and rename djj.sh to dj1.sh

--- dj1.sh
+++ dj1.sh
@@ -1,11 +1,6 @@
 apt install python3 python3-pip -y
 pip install requests beautifulsoup4
 
-read -p "用户名:" mima
-read -p "密码:" mi
-os.environ['USERNAME']='$mima'
-os.environ['PASSWORD']='$mi'
-
 wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main.py
 chmod +x main.py
 python3 main.py
