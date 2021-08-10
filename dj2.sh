a453a1afb98ee20fe99c282370cf4dee437c2c91
甬哥 <80431714+YG-tsj@users.noreply.github.com>
2021-08-10 12:50:47 +0800

    Update and rename dj.sh to dj2.sh

--- dj2.sh
+++ dj2.sh
@@ -5,6 +5,6 @@ apt-get install cron
 echo "* * * * * /usr/bin/python3 /root/main.py >/dev/null  2>&1" >> /var/spool/cron/crontabs/root
 service cron restart
 
-wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main1.py
-chmod +x main1.py
-python3 main1.py
+wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main2.py
+chmod +x main2.py
+python3 main2.py
