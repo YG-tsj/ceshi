ad64dc82890895dac9ebcc994c9b08e9858fee9c
甬哥 <80431714+YG-tsj@users.noreply.github.com>
2021-08-10 13:17:29 +0800

    Update and rename dj2.sh to dj3.sh

--- dj3.sh
+++ dj3.sh
@@ -2,9 +2,11 @@ apt update
 apt install python3 python3-pip -y
 pip install requests beautifulsoup4
 apt-get install cron 
-echo "* * * * * /usr/bin/python3 /root/main.py >/dev/null  2>&1" >> /var/spool/cron/crontabs/root
-service cron restart
+
 
 wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main2.py
-chmod +x main2.py
-python3 main2.py
+chmod +x main3.py
+python3 main3.py
+
+echo "* * * * * /usr/bin/python3 /root/main3.py >/dev/null  2>&1" >> /var/spool/cron/crontabs/root
+service cron restart
