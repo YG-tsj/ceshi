26ed4c4cc52fc39ef34aff7b84633f2c9b203678
甬哥 <80431714+YG-tsj@users.noreply.github.com>
2021-08-11 09:32:42 +0800

    Update dj4.sh

--- dj4.sh
+++ dj4.sh
@@ -8,6 +8,7 @@ wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/YG-tsj/ceshi/main3.py
 chmod +x main3.py
 python3 main3.py
 
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 crontab -l
 echo "* * * * * /usr/bin/python3 /root/main3.py >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
 service cron restart
